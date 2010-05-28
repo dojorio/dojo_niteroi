75cc4441c613827c118bc22ac87b29441dd125a1
Bernardo Fontes <bernardoxhc@gmail.com>
2010-05-28 20:08:25 -0300

    Refactoring

--- javascript_jokenpo/jokenpo.js
+++ javascript_jokenpo/jokenpo.js
@@ -1,15 +1,30 @@
 function jokenpo (jogada1, jogada2) {
     var tabelaDeJogadas = {
-    "pedra":{"pedra":"empate","papel":"papel","tesoura":"pedra"},
-    "papel":{"pedra":"papel","papel":"empate","tesoura":"tesoura"},
-    "tesoura":{"pedra":"pedra","papel":"tesoura","tesoura":"empate"}
+    "pedra":{
+        "pedra":"empate",
+        "papel":"papel",
+        "tesoura":"pedra"
+    },
+    "papel":{
+        "pedra":"papel",
+        "papel":"empate",
+        "tesoura":"tesoura"
+    },
+    "tesoura":{
+        "pedra":"pedra",
+        "papel":"tesoura",
+        "tesoura":"empate"
+    }
     };
     return tabelaDeJogadas[jogada1][jogada2];
 }
 
 function jogadorJokenpo(jogador1, jogada1, jogador2, jogada2)
 {
+
     if(jogada2 == "tesoura")
         return "Pedro";
+    if(jogada2 == "pedra")
+        return "empate;"
     return "João";
 }
