90a6311c9fb60093f40045c980202ee32942e276
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-15 12:11:01 -0300

    Tue Jun 15 12:11:01 2010

--- 20100615_ruby_bolao_copa/bolao.rb
+++ 20100615_ruby_bolao_copa/bolao.rb
@@ -1,3 +1,19 @@
+class Resultado
+
+  def initialize(gols_time_a, gols_time_b)
+    @gols_time_a = gols_time_a
+    @gols_time_b = gols_time_b
+  end
+
+  def pontuacao_resultado(resultado_final)
+    if @gols_time_b == resultado_final.gols_time_b
+      return 1
+    end
+    return 0
+  end
+
+end
+
 def bolao(apostador1, aposta, placar)
   return [apostador1, 0] if aposta[2] != placar[2]
   [apostador1, 1]
