346675f2021268e433a7a188946931dae8ec7222
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-15 11:58:17 -0300

    Tue Jun 15 11:58:17 2010

--- 20100615_ruby_bolao_copa/spec_bolao.rb
+++ 20100615_ruby_bolao_copa/spec_bolao.rb
@@ -3,7 +3,11 @@ require 'spec'
 require 'bolao'
 
 describe 'bolao' do
-  it 'apostador 1 nao acerta nada' do
+  it '"Mario" nao acerta nada' do
+    apostador1 = "Mario"
+    bolao(apostador1, "0x0", "2x1").should == [apostador1, 0]
+  end
+  it '"Mario" nao acerta nada' do
     apostador1 = "Mario"
     bolao(apostador1, "0x0", "2x1").should == [apostador1, 0]
   end
