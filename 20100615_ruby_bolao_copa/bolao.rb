86c6a25ca8fb0489b7dced4ea394b7f0920447e3
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-15 12:33:00 -0300

    Tue Jun 15 12:32:59 2010

--- 20100615_ruby_bolao_copa/bolao.rb
+++ 20100615_ruby_bolao_copa/bolao.rb
@@ -1,13 +1,15 @@
 class Resultado
 
-  attr_accessor "gols_time_a"
-  attr_accessor "gols_time_b"
+  attr_accessor "gols_time_a","gols_time_b"
   def initialize(gols_time_a, gols_time_b)
     @gols_time_a = gols_time_a
     @gols_time_b = gols_time_b
   end
 
   def pontuacao_resultado(resultado_final)
+    if @gols_time_a == resultado_final.gols_time_a
+      return 1
+    end
     if @gols_time_b == resultado_final.gols_time_b
       return 1
     end
