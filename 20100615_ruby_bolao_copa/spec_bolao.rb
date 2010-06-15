427bc3a7f1cfc55cf7a0e810dd92e01b7b3f1807
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-15 12:13:38 -0300

    Tue Jun 15 12:13:38 2010

--- 20100615_ruby_bolao_copa/spec_bolao.rb
+++ 20100615_ruby_bolao_copa/spec_bolao.rb
@@ -3,12 +3,21 @@ require 'spec'
 require 'bolao'
 
 describe 'bolao' do
+
+  it 'deve retornar 0 pontos quando toda aposta estiver errada' do
+    aposta = Resultado.new(0, 0)
+    resultado_final = Resultado.new(2, 1)
+    aposta.pontuacao_resultado(resultado).should == 0
+  end
+
   it '"Mario" nao acerta nada' do
     apostador1 = "Mario"
     bolao(apostador1, "0x0", "2x1").should == [apostador1, 0]
   end
+
   it '"Mario" acerta um resultado' do
     apostador1 = "Mario"
     bolao(apostador1, "0x1", "2x1").should == [apostador1, 1]
   end
+
 end
