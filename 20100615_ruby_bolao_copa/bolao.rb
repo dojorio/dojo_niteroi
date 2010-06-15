11c61336525fb5fef1ffafe7e3a0bc265fe61c08
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-15 12:55:02 -0300

    Tue Jun 15 12:55:02 2010

--- 20100615_ruby_bolao_copa/bolao.rb
+++ 20100615_ruby_bolao_copa/bolao.rb
@@ -14,9 +14,16 @@ class Resultado
     return @gols_time_b == resultado.gols_time_b
   end
 
+  def empate?
+    return @gols_time_b == @gols_time_a
+  end
+
   def pontuacao_resultado(resultado_final)
     if acertou_gols_time_a(resultado_final) || acertou_gols_time_b(resultado_final)
       return 1
+    end
+    if empate? == resultado_final.empate?
+      return 2
     end
     return 0
   end
