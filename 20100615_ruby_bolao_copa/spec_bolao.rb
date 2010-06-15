0952ce3f32fce4577171d5c7b54fac309bae7448
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-15 11:54:12 -0300

    Tue Jun 15 11:54:12 2010

--- 20100615_ruby_bolao_copa/spec_bolao.rb
+++ 20100615_ruby_bolao_copa/spec_bolao.rb
@@ -3,7 +3,7 @@ require 'spec'
 require 'bolao'
 
 describe 'bolao' do
-  it 'apostador 1 não acerta nada' do
+  it 'apostador 1 nao acerta nada' do
     bolao(apostador1, "0x0", "2x1").should == [apostador1, 0]
   end
 end
