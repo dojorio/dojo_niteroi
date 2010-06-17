41132cd9af68124fecef16dece211bf43c3f9307
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-17 19:53:06 -0300

    Thu Jun 17 19:53:06 2010

--- 20100617_javascript_happy_numbers/happy_numbers.js
+++ 20100617_javascript_happy_numbers/happy_numbers.js
@@ -1,7 +1,9 @@
 function happyNumber(number){
 
-    if (number == 1 || number == 7)
+    if (number == 1 || number == 7 || number == 10){
         return "I'm happy";
+    }
     return "I'm sad";
+
 
 }
