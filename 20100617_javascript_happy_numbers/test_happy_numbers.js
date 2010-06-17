1dad53bc38b1cf60b627c830d6991264bd4cc2f5
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-17 19:42:01 -0300

    Thu Jun 17 19:42:01 2010

--- 20100617_javascript_happy_numbers/test_happy_numbers.js
+++ 20100617_javascript_happy_numbers/test_happy_numbers.js
@@ -3,7 +3,7 @@ var sad = "I'm sad";
 describe('Happy Numbers', {
 
     'return "I\'m happy" for 1': function() {
-        value_of(happy(1)).should_be(happy);
+        value_of(happyNumber(1)).should_be(happy);
     },
 
     'return "I\'m sad" for 2': function() {
