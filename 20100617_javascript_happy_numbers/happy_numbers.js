101481beccd28750a99623151fb76c075f9f8e81
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-17 20:11:16 -0300

    Thu Jun 17 20:11:15 2010

--- 20100617_javascript_happy_numbers/happy_numbers.js
+++ 20100617_javascript_happy_numbers/happy_numbers.js
@@ -8,11 +8,11 @@ function happyNumber(number){
 }
 
 function splitNumber(number){
-    var _tmp = number  + "";
+    var _tmp = new String(number);
     var _return = new Array();
     for (var i = 0; i < _tmp.length; i++)
     {
-        _return.push(_tmp[i]/1);
+        _return.push(parseInt(_tmp[i]));
     }
     return _return;
 }
