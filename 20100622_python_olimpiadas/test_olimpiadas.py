9e364ada8dae623f4a7ed1c6282fea86952149bf
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-22 11:37:00 -0300

    Tue Jun 22 11:37:00 2010

--- 20100622_python_olimpiadas/test_olimpiadas.py
+++ 20100622_python_olimpiadas/test_olimpiadas.py
@@ -5,7 +5,7 @@ class TestOlimpiadas(unittest.TestCase):
     def test_1_pais_retorna_mesmo_pais(self):
         tupla_brasil = ("Brasil" ,1 , 0, 0)
         entrada = [tupla_brasil]
-        self.assertEquals("Brasil", )
+        self.assertEquals(["Brasil"], calcula_ranking(entrada))
 
 if __name__ == '__main__':
     unittest.main()
