7cdac4687c88e6919ee63a20d4a285760157a25f
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-29 11:49:19 -0300

    Tue Jun 29 11:49:19 2010

--- 20100629_python_jogo_da_velha/jogo_da_velha.py
+++ 20100629_python_jogo_da_velha/jogo_da_velha.py
@@ -1,4 +1,18 @@
 def avalia_jogo(entrada):
+    return avalia_linhas(entrada)
+
+
+
+
+
+
+
+    if entrada[0][0] == entrada[0][1] == entrada[0][2]:
+        return entrada[0][0]
+    if entrada[1][0] == entrada[1][1] == entrada[1][2]:
+        return entrada[1][0]
+
+def avalia_linhas(entrada):
     if entrada[0][0] == entrada[0][1] == entrada[0][2]:
         return entrada[0][0]
     if entrada[1][0] == entrada[1][1] == entrada[1][2]:
