256ba2cce941f88bede35085e226bcd5057d40e8
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-29 11:51:51 -0300

    Tue Jun 29 11:51:51 2010

--- 20100629_python_jogo_da_velha/jogo_da_velha.py
+++ 20100629_python_jogo_da_velha/jogo_da_velha.py
@@ -5,10 +5,3 @@ def avalia_linhas(entrada):
     for i in range(3):
         if entrada[i][0] == entrada [i][1] == entrada[i][2]:
             return entrada[i][0]
-
-
-
-    if entrada[0][0] == entrada[0][1] == entrada[0][2]:
-        return entrada[0][0]
-    if entrada[1][0] == entrada[1][1] == entrada[1][2]:
-        return entrada[1][0]
