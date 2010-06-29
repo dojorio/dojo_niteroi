a8fa94bc1f9f0cacd78b8f63eecae699c4d9ccd7
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-29 11:09:37 -0300

    Adicionando arquivos de código

--- 20100622_python_olimpiadas/olimpiadas.py
+++ 20100622_python_olimpiadas/olimpiadas.py
@@ -1,2 +1,18 @@
-def calcula_ranking(lista_de_paises)
-    return ["Brasil"]
+class Pais (object):
+    nome = ""
+    ouro = 0
+    prata = 0
+    bronze = 0
+
+    def __init__(self, nome, ouro, prata, bronze):
+        self.nome = nome
+        self.ouro = ouro
+        self.prata = prata
+        self.bronze = bronze
+
+    def __lt__(self, pais):
+        return self.ouro < pais.ouro
+
+def calcula_ranking(paises):
+    ordenada = sorted(paises, reverse = True)
+    return [pais.nome for pais in ordenada]
--- 20100622_python_olimpiadas/test_olimpiadas.py
+++ 20100622_python_olimpiadas/test_olimpiadas.py
@@ -1,12 +1,28 @@
 import unittest
-from olimpiadas import calcula_ranking
+from olimpiadas import calcula_ranking, Pais
 
 class TestOlimpiadas(unittest.TestCase):
 
+    def setUp(self):
+        self.brasil = Pais("Brasil", 1, 0, 0)
+        self.eua = Pais("EUA", 0, 0, 0)
+        self.cuba = Pais("Cuba", 2, 0, 0)
+
     def test_1_pais_retorna_mesmo_pais(self):
-        tupla_brasil = ("Brasil" ,1 , 0, 0)
-        entrada = [tupla_brasil]
+        entrada = [self.brasil]
         self.assertEquals(["Brasil"], calcula_ranking(entrada))
+
+    def test_Brasil_EUA_retorna_Brasil_EUA(self):
+        entrada = [self.brasil, self.eua]
+        self.assertEquals(["Brasil", "EUA"], calcula_ranking(entrada))
+
+    def test_EUA_Brasil_retorna_Brasil_EUA(self):
+        entrada = [self.eua, self.brasil]
+        self.assertEquals(["Brasil", "EUA"], calcula_ranking(entrada))
+
+    def test_EUA_Brasil_Cuba_retorna_Cuba_Brasil_EUA(self):
+        entrada = [self.eua, self.brasil, self.cuba]
+        self.assertEquals(["Cuba", "Brasil", "EUA"], calcula_ranking(entrada))
 
 if __name__ == '__main__':
     unittest.main()
