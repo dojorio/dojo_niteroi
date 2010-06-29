316871e3f95a9d33b604de2d37278a98eb4aaa3f
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-29 12:25:17 -0300

    Tue Jun 29 12:25:17 2010

--- 20100629_python_jogo_da_velha/jogo_da_velha.py
+++ 20100629_python_jogo_da_velha/jogo_da_velha.py
@@ -5,7 +5,7 @@ def avalia_jogo(jogo):
 
 def avalia_linhas(jogo):
     for linha in jogo:
-        if not linha[0]:
-            continue
-        if linha[0] == linha[1] == linha[2] :
+        #if not linha[0]:
+        #    continue
+        if linha[0] == linha[1] == linha[2] !='':
             return linha[0]
