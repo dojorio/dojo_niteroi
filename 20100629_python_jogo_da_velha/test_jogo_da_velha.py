9457b833961a861b5922b8746f34aa2b3064cbca
Bernardo Fontes <bernardoxhc@gmail.com>
2010-06-29 11:24:47 -0300

    Tue Jun 29 11:24:47 2010

--- 20100629_python_jogo_da_velha/test_jogo_da_velha.py
+++ 20100629_python_jogo_da_velha/test_jogo_da_velha.py
@@ -4,7 +4,12 @@ from jogo_da_velha import avalia_jogo
 class TestJogoDaVelha(unittest.TestCase):
 
     def test_method(self):
-        self.assertTrue(True)
+        entrada = [
+            ('X', 'X', 'X'),
+            ('0', '', ''),
+            ('', '0', ''),
+        ]
+        self.assertEquals(avalia_jogo(entrada), 'X')
 
 if __name__ == '__main__':
     unittest.main()
