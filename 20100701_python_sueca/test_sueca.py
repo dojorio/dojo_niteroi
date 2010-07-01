f8262b10dbc44d5e936d728e9a85b2bb81876aaa
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 19:28:28 -0300

    Thu Jul  1 19:28:28 2010

--- 20100701_python_sueca/test_sueca.py
+++ 20100701_python_sueca/test_sueca.py
@@ -3,13 +3,6 @@ from sueca import Carta
 
 class TestValoresCartas(unittest.TestCase):
 
-    def test_A_vale_11_pontos(self):
-        carta = Carta('Copas', 'A')
-        self.assertEquals(carta.valor, 11)
-
-    def test_7_vale_10_pontos(self):
-        carta = Carta('Copas', '7')
-        self.assertEquals(carta.valor, 10)
 
 if __name__ == '__main__':
     unittest.main()
