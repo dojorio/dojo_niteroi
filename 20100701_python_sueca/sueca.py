c0c0c3212b326ff4317ca6160fbc7a65056e9e35
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 19:51:00 -0300

    Thu Jul  1 19:51:00 2010

--- 20100701_python_sueca/sueca.py
+++ 20100701_python_sueca/sueca.py
@@ -1,8 +1,17 @@
 class Carta(object):
-    def __init__(self, naipe, nome, valor):
+    def __init__(self, naipe, nome):
         self.naipe = naipe
         self.nome = nome
-        self.valor = valor
+        self.valor = 0
+        calcula_valor()
+
+    def calcula_valor():
+        valores = { '2':0,
+                    '3':0,
+                    '4':0,
+                    '5':0}
+        self.valor = valores[self.nome]
+
 
 def somatorio(cartas):
     return 0
