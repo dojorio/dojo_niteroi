fac67d3165c3f36c162401aee5623667182739ca
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 19:41:44 -0300

    Thu Jul  1 19:41:44 2010

--- 20100701_python_sueca/test_sueca.py
+++ 20100701_python_sueca/test_sueca.py
@@ -4,14 +4,12 @@ from sueca import somatorio
 
 class TestSomatorioMesa(unittest.TestCase):
     def test_tudo_zero(self):
-        carta1 = Carta('Copas', '2', 0)
-        carta2 = Carta('Copas', '3', 0)
-        carta3 = Carta('Copas', '4', 0)
-        carta4 = Carta('Copas', '5', 0)
-        self.assertEquals(somatorio([carta1,
-                                    carta2,
-                                    carta3,
-                                    carta4]), 0)
+        cartas = [
+        Carta('Copas', '2', 0),
+        Carta('Copas', '3', 0),
+        Carta('Copas', '4', 0),
+        Carta('Copas', '5', 0)]
+        self.assertEquals(somatorio(cartas), 0)
 
 
 if __name__ == '__main__':
