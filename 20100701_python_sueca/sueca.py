91ecceecfc2e0e86f14198347171a3e817aea7f6
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 20:55:26 -0300

    Thu Jul  1 20:55:26 2010

--- 20100701_python_sueca/sueca.py
+++ 20100701_python_sueca/sueca.py
@@ -19,7 +19,7 @@ class Carta(object):
         self.valor = valores[self.nome]
 
     def __gt__(self, carta):
-        self.valor > carta.valor
+        return self.valor > carta.valor
 
 def somatorio(cartas):
     return sum([carta.valor for carta in cartas])
@@ -28,10 +28,14 @@ def rodada(jogadas, trunfo):
     lista_de_cartas = [carta[1] for carta in jogadas]
 
     pontos_vencedor = somatorio(lista_de_cartas)
-    vencedor = ""
 
-    for dupla in jogadas:
-        if dupla[1].valor == pontos_vencedor:
-            vencedor = dupla[0]
+
+    maior_carta = max(lista_de_cartas)
+    indice_do_vencedor = lista_de_cartas.index(maior_carta)
+    vencedor = jogadas[indice_do_vencedor][0]
+   # for dupla in jogadas:
+       # if dupla[1].valor == pontos_vencedor:
+        #    vencedor = dupla[0]
+
 
     return (vencedor, pontos_vencedor)
