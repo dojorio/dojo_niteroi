88de7526297270b7578f95df1ba19f352b081418
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 20:00:40 -0300

    Thu Jul  1 20:00:40 2010

--- 20100701_python_sueca/sueca.py
+++ 20100701_python_sueca/sueca.py
@@ -1,15 +1,13 @@
-class Carta(object):
+valores = { '2':0,
+            '3':0,
+            '4':0,
+            '5':0}
+
+class CartaSueca(object):
+
     def __init__(self, naipe, nome):
         self.naipe = naipe
         self.nome = nome
-        self.valor = 0
-        self.calcula_valor()
-
-    def calcula_valor(self):
-        valores = { '2':0,
-                    '3':0,
-                    '4':0,
-                    '5':0}
         self.valor = valores[self.nome]
 
 
