f860ab2c8f5e1b8cabe913e004ab346293ade84a
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 20:12:36 -0300

    Thu Jul  1 20:12:36 2010

--- 20100701_python_sueca/test_sueca.py
+++ 20100701_python_sueca/test_sueca.py
@@ -2,13 +2,26 @@ import unittest
 from sueca import Carta
 from sueca import somatorio
 
+class TestGanhadorDaRodada(unittest.TestCase):
+
+    def test_jogador_1_ganha_com_A_copas_trunfo_Copas(self):
+        cartas = [
+            Carta('Copas', 'A'),
+            Carta('Copas', '3'),
+            Carta('Copas', '4'),
+            Carta('Copas', '5'),
+        ]
+        trunfo = "Copas"
+        self.assertEquals(rodada(cartas, trunfo))
+
 class TestSomatorioMesa(unittest.TestCase):
     def test_2_3_4_5_retorna_0(self):
         cartas = [
             Carta('Copas', '2'),
             Carta('Copas', '3'),
             Carta('Copas', '4'),
-            Carta('Copas', '5')]
+            Carta('Copas', '5'),
+        ]
         self.assertEquals(somatorio(cartas), 0)
 
     def test_A_3_4_5_retorna_11(self):
