11e2dc3d1b8260f2f03ef9a2c474072967d0518e
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 20:08:19 -0300

    Thu Jul  1 20:08:19 2010

--- 20100701_python_sueca/sueca.py
+++ 20100701_python_sueca/sueca.py
@@ -1,9 +1,14 @@
 valores = {
+    'A':11,
+    '7':10,
+    'K':4,
+    'J':3,
+    'Q':2,
     '2':0,
     '3':0,
     '4':0,
     '5':0,
-    'A':11,
+    '6':0,
 }
 
 class Carta(object):
@@ -12,7 +17,6 @@ class Carta(object):
         self.naipe = naipe
         self.nome = nome
         self.valor = valores[self.nome]
-
 
 def somatorio(cartas):
     return sum([carta.valor for carta in cartas])
