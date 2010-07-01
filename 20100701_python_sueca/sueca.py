a3be45d954f8eb0626bbe70a95a0c4203e904dbd
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 19:16:53 -0300

    Thu Jul  1 19:16:53 2010

--- 20100701_python_sueca/sueca.py
+++ 20100701_python_sueca/sueca.py
@@ -2,6 +2,8 @@ class Carta(object):
     def __init__(self, naipe, carta):
         self.naipe = naipe
         self.carta = carta
+    def valor:
+        return 11;
 
 def pontuacao():
     return ''
