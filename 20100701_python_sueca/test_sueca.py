976e8d56cf13ec2e0c540b21109ead484c425d23
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-01 19:33:53 -0300

    Thu Jul  1 19:33:53 2010

--- 20100701_python_sueca/test_sueca.py
+++ 20100701_python_sueca/test_sueca.py
@@ -1,7 +1,14 @@
 import unittest
 from sueca import Carta
 
-class TestValoresCartas(unittest.TestCase):
+class TestSomatorioMesa(unittest.TestCase):
+    def test_tudo_zero(self):
+        carta1 = Carta('Copas', '2')
+        carta2 = Carta('Copas', '3')
+        carta3 = Carta('Copas', '4')
+        carta4 = Carta('Copas', '5')
+        self.assertEquals(somatorio(carta1, carta2,
+                                    carta3, carta4), 0)
 
 
 if __name__ == '__main__':
