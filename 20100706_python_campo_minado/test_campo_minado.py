5d2bc9ecb7cedb0fa58d8c0827edb8bc7613ce88
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-06 11:33:56 -0300

    Tue Jul  6 11:33:56 2010

--- 20100706_python_campo_minado/test_campo_minado.py
+++ 20100706_python_campo_minado/test_campo_minado.py
@@ -7,9 +7,10 @@ class TestCampoMinado(unittest.TestCase):
     def test_2_x_2_sem_minas(self):
 
         campo = CampoMinado([
-            ['-', '-'],
-            ['-', '-'],
-        ])
+                ['-', '-'],
+                ['-', '-'],
+            ]
+        )
         saida = [
             [0, 0],
             [0, 0],
@@ -18,15 +19,16 @@ class TestCampoMinado(unittest.TestCase):
         self.assertEquals(campo.solucao(), saida)
 
     def test_2_x_2_1_mina_canto_superior_esquerdo(self):
-        entrada = [
-            ['*', '-'],
-            ['-', '-'],
-        ]
+        campo = CampoMinado([
+                ['*', '-'],
+                ['-', '-'],
+            ]
+        )
         saida = [
             ['*',1],
             [1, 1],
         ]
-        self.assertEquals(resultado(entrada), saida)
+        self.assertEquals(campo.solucao(), saida)
 
 if __name__ == '__main__':
     unittest.main()
