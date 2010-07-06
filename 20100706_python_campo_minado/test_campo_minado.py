a1932f1114f02832f327a7dfd15c6e0e8cf14e29
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-06 11:07:54 -0300

    Tue Jul  6 11:07:54 2010

---
+++ 20100706_python_campo_minado/test_campo_minado.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestCampoMinado(unittest.TestCase):
+
+    def test_2_x_2_sem_minas(self):
+        entrada = [
+            ['-', '-'],
+            ['-', '-'],
+        ]
+        saida = [
+            [0, 0],
+            [0, 0],
+        ]
+        self.assertEquals(resultado(entrada), saida)
+
+if __name__ == '__main__':
+    unittest.main()
