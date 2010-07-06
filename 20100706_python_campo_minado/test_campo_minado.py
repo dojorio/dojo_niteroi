7c5eb9654a275bbaf25114ed5fe8ec3bde341d19
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-06 11:34:17 -0300

    Tue Jul  6 11:34:16 2010

--- 20100706_python_campo_minado/test_campo_minado.py
+++ 20100706_python_campo_minado/test_campo_minado.py
@@ -4,7 +4,6 @@ from campo_minado import CampoMinado
 class TestCampoMinado(unittest.TestCase):
 
     def test_2_x_2_sem_minas(self):
-
         campo = CampoMinado([
                 ['-', '-'],
                 ['-', '-'],
