34d1d1cbb7ed3072df11640bfb1f272308930669
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-06 12:25:21 -0300

    Tue Jul  6 12:25:21 2010

--- 20100706_python_campo_minado/campo_minado.py
+++ 20100706_python_campo_minado/campo_minado.py
@@ -1,13 +1,13 @@
 class CampoMinado(object):
 
-    def __init__(self, campo_entrada):
-        lista_linhas = campo_entrada.splitlines()
-        campo_lista = []
-        for linha in lista_linhas:
+    def __init__(self, minas):
+        linhas = minas.splitlines()
+        campo = []
+        for linha in linhas:
             linha = linha.strip()
             if linha:
-                campo_lista.append([caracter for caracter in linha])
-        self.campo = campo_lista
+                campo.append([caracter for caracter in linha])
+        self.campo = campo
 
     def solucao(self):
         if self.campo[0][0] == '*':
