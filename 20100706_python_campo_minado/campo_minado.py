d75ccca80cca6aa2c65bc85c942ed2349422443d
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-06 11:31:58 -0300

    Tue Jul  6 11:31:58 2010

--- 20100706_python_campo_minado/campo_minado.py
+++ 20100706_python_campo_minado/campo_minado.py
@@ -2,6 +2,18 @@ class CampoMinado(object):
     def __init__(self, campo):
         self.campo=campo
 
+    def solucao(self):
+       if self.campo[0][0] == '*':
+        return[
+            ['*', 1],
+            [1, 1],
+        ]
+
+
+        return [
+          [0, 0],
+          [0, 0],
+        ]
 
 
 
