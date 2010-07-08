f686d98bc9e2168a2992a05a555141caa823525c
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-08 19:54:43 -0300

    Thu Jul  8 19:54:43 2010

--- 20100708_ruby_sinuca/sinuca_rspec.rb
+++ 20100708_ruby_sinuca/sinuca_rspec.rb
@@ -22,4 +22,12 @@ describe Jogada do
     matou = false
     Jogada.new(bola_da_vez, bola_alvo, acertou, matou).pontos().should == -1
   end
+
+  it 'erra a bola da vez e nao mata perde o ponto da bola da vez' do
+    bola_da_vez = 2
+    bola_alvo = bola_da_vez
+    acertou = false
+    matou = false
+    Jogada.new(bola_da_vez, bola_alvo, acertou, matou).pontos().should == -2
+  end
 end
