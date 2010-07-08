89037990152884b1caf863298550291d454b76dc
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-08 19:52:56 -0300

    Thu Jul  8 19:52:56 2010

--- 20100708_ruby_sinuca/sinuca.rb
+++ 20100708_ruby_sinuca/sinuca.rb
@@ -1,13 +1,24 @@
 class Jogada
 
-  attr_accessor :pontos
+  @bola_da_vez
+  @bola_alvo
+  @acertou
+  @matou
 
   def initialize(bola_da_vez, bola_alvo, acertou, matou)
+    @bola_da_vez = bola_da_vez
+    @bola_alvo = bola_alvo
+    @acertou = acertou
+    @matou = matou
+  end
+
+  def pontos
     if acertou
       @pontos = 0
     else
       @pontos = -1
     end
   end
+
 
 end
