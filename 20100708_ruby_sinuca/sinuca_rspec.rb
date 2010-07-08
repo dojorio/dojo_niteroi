4daf262393b914bba2d0205adf0fa0641532d022
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-08 19:37:00 -0300

    Thu Jul  8 19:37:00 2010

--- 20100708_ruby_sinuca/sinuca_rspec.rb
+++ 20100708_ruby_sinuca/sinuca_rspec.rb
@@ -6,8 +6,10 @@ require 'rubygems'
 require 'spec'
 require 'sinuca'
 
-describe Problema do
-  it 'test pass' do
-    Problema.new.soma(1, 1).should == 2
-  end
-end
+describe Jogada do
+  it 'acerta a bola da vez e nao mata deve retornar 0' do
+    bola_da_vez = 1
+    bola_alvo = bola_da_vez
+    acertou = true
+    matou = false
+    Jogada.new(bola_da_vez, bola_alvo, acertou, matou).pontos.should == 0
