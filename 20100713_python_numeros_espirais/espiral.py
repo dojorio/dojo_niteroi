6d31473454fd8b91bd2930b3a8c7be64f69c3875
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-13 12:08:38 -0300

    Tue Jul 13 12:08:38 2010

--- 20100713_python_numeros_espirais/espiral.py
+++ 20100713_python_numeros_espirais/espiral.py
@@ -7,15 +7,15 @@ def numeros_espirais(ordem):
     i, j = 0, 0
 
     for numero in range(1, limite + 1):
+        if i == 1 and j >= 0:
+            matriz[i][j] = numero
+            j -= 1
         if i == 0 and j < ordem:
             matriz[i][j] = numero
             j += 1
         else:
             i += 1
             j = ordem - 1
-        if i == 1 and j >= 0:
-            matriz[i][j] = numero
-            j -= 1
 
     return matriz
 
