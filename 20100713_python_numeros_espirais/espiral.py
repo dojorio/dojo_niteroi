cd782dab709d0d3da26fd1047eaf36a9b6c42f1d
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-13 11:59:14 -0300

    Tue Jul 13 11:59:14 2010

--- 20100713_python_numeros_espirais/espiral.py
+++ 20100713_python_numeros_espirais/espiral.py
@@ -1,4 +1,33 @@
 def numeros_espirais(ordem):
+
+
+    linha = [0] * ordem
+    matriz = [linha] * ordem
+    limite = ordem ** 2
+    x, y = 0, 0
+    incx, incy = 1 , 0
+
+    valoressalvos = []
+    for i in range(1, limite + 1):
+        matriz[y][x] = i
+        x += incx
+        y += incy
+        if x == ordem - 1:
+            incy = 1
+            incx = 0
+        if y == ordem - 1:
+            incy = 0
+            incx = -1
+        if x == 0:
+            incy = -1
+            incx = 0
+        if matriz [x+incx, y+incy] == 0:
+            incy = 0
+            incx = 1
+
+
+
+
     if ordem == 1:
         return [[1]]
     return [[1,2],
