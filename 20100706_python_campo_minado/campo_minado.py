b7e704328f5600955b10ff578c8736209629e3c7
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-12 23:40:37 -0300

    Problema do Campo Minado

--- 20100706_python_campo_minado/campo_minado.py
+++ 20100706_python_campo_minado/campo_minado.py
@@ -6,13 +6,19 @@ class CampoMinado(object):
         for linha in linhas:
             linha = linha.strip()
             if linha:
-                campo = campo.replace('-', '0')
-                campo.append([caracter for caracter in linha])
+                linha = linha.replace('-', '0')
+                lista_de_campo = []
+                for caracter in linha:
+                    try:
+                        lista_de_campo.append(int(caracter))
+                    except Exception:
+                        lista_de_campo.append(caracter)
+                campo.append(lista_de_campo)
         self.campo = campo
 
     def solucao(self):
         if self.campo[0][0] == '*':
-            if self.campo[0][1] == '-':
+            if self.campo[0][1] == 0:
                 return[
                     ['*', 1],
                     [1, 1],
