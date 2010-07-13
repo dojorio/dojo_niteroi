5eb829fe91c7defe9f1adb0b8a00ef6f350f0a19
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-13 11:31:46 -0300

    Tue Jul 13 11:31:46 2010

---
+++ 20100713_python_numeros_espirais/test_espiral.py
@@ -0,0 +1,9 @@
+from unittest
+
+class TestEspiral(unittest.TestCase):
+
+    def test_matriz_1_x_1(self):
+        self.assertEqual(numeros_espirais(1), [[1]])
+
+if __name__ == '__main__':
+    unittest.main()
