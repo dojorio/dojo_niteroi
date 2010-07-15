4ae88337617511d83c4e32308e6504646459d7e9
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-15 19:42:42 -0300

    Thu Jul 15 19:42:42 2010

--- 20100715_python_verificador_numeros_espirais/espiral.py
+++ 20100715_python_verificador_numeros_espirais/espiral.py
@@ -1,4 +1,12 @@
-def verifica_matriz_espiral(matriz):
-    if len(matriz[0]) == 2:
-        return matriz[0][1] == matriz[0][0] + 1
-    return True
+class Matriz
+    matriz = [[]]
+    def elemento(self, i, j):
+        return matriz[i][j]
+
+    def define_matriz(nova_matriz):
+        matriz = nova_matriz
+
+    def e_matriz_espiral(matriz):
+        if len(matriz[0]) == 2:
+            return matriz[0][1] == matriz[0][0] + 1
+        return True
