13e374b51d841cf2d3c812523217b03c54559fca
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-15 19:34:24 -0300

    Thu Jul 15 19:34:24 2010

--- 20100715_python_verificador_numeros_espirais/espiral.py
+++ 20100715_python_verificador_numeros_espirais/espiral.py
@@ -1,4 +1,4 @@
 def verifica_matriz_espiral(matriz):
-    if len(matriz[0]) == 2 and matriz[0][1] != matriz[0][0] + 1:
-        return False
+    if len(matriz[0]) == 2:
+        return matriz[0][1] == matriz[0][0] + 1
     return True
