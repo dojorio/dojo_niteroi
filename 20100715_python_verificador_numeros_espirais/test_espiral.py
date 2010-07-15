c71aab3275321282041147ea81f57c448e12841b
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-15 20:13:34 -0300

    Thu Jul 15 20:13:34 2010

--- 20100715_python_verificador_numeros_espirais/test_espiral.py
+++ 20100715_python_verificador_numeros_espirais/test_espiral.py
@@ -3,7 +3,7 @@ from espiral import Matriz, lista_ordenada
 
 class TestVerificaListaOrdenada(unittest.TestCase):
 
-    def test_linha_esta_ordenada(self)
+    def test_linha_esta_ordenada(self):
         self.assertTrue(lista_ordenada([1])
 
 
