ec3147ea553f97626a882138796c39c1baf728b9
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-15 19:54:37 -0300

    Thu Jul 15 19:54:37 2010

--- 20100715_python_verificador_numeros_espirais/test_espiral.py
+++ 20100715_python_verificador_numeros_espirais/test_espiral.py
@@ -4,25 +4,24 @@ from espiral import Matriz
 class TestVerificaMatrizEspiral(unittest.TestCase):
 
     def test_matriz_1_x_1_retorna_true(self):
-        matriz = [
+        matriz = Matriz([
             [1],
-        ]
-        teste = Matriz(matriz)
-        self.assertTrue(teste.e_matriz_espiral())
+        ])
+        self.assertTrue(matriz.e_matriz_espiral())
 
     def test_matriz_1_x_2_retorna_true(self):
-        matriz = [
+        matriz = Matriz([
             [1, 2],
-        ]
+        ])
         teste = Matriz(matriz)
-        self.assertTrue(teste.e_matriz_espiral())
+        self.assertTrue(matriz.e_matriz_espiral())
 
     def test_matriz_1_x_2_retorna_false(self):
-        matriz = [
+        matriz = Matriz([
             [1, 3],
-        ]
+        ])
         teste = Matriz(matriz)
-        self.assertFalse(teste.e_matriz_espiral())
+        self.assertFalse(matriz.e_matriz_espiral())
 
 if __name__ == '__main__':
     unittest.main()
