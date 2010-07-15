cc42bfbc824f55b6777faec59343087b9aaf082b
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-15 19:23:46 -0300

    Thu Jul 15 19:23:45 2010

--- 20100715_python_verificador_numeros_espirais/test_espiral.py
+++ 20100715_python_verificador_numeros_espirais/test_espiral.py
@@ -1,1 +1,9 @@
+import unittest
 
+class TestVerificaMatrizEspiral(unittest.TestCase):
+
+    def test_matriz_1_x_1_retorna_true(self):
+        matriz = [
+            [1],
+        ]
+        self.assertTrue(verifica_matriz_espiral(matriz))
