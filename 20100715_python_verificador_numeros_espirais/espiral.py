930d0759b38fc209695b22cd1a10a00c1425d48f
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-15 21:00:59 -0300

    Thu Jul 15 21:00:59 2010

--- 20100715_python_verificador_numeros_espirais/espiral.py
+++ 20100715_python_verificador_numeros_espirais/espiral.py
@@ -11,7 +11,7 @@ class Matriz(object):
         return len(self.matriz[0])
 
     @property
-    def num_linhas(self)
+    def num_linhas(self):
         return len(self.matriz)
 
     def ultima_linha(self):
@@ -21,6 +21,8 @@ class Matriz(object):
         return self.matriz[i]
 
     def e_matriz_espiral(self):
+        if self.num_linhas == 1:
+            return lista_ordenada_sequencialmente(self.linha(0))
         if self.num_colunas == 2:
             return lista_ordenada_sequencialmente(self.linha(0)) and lista_ordenada_sequencialmente(self.ultima_linha(), reverso=True)
         return True
