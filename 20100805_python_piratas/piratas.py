3923ecf24d5f67abecdf07999a5af9a07a2c8c9e
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-05 20:20:14 -0300

    Thu Aug  5 20:20:14 2010

--- 20100805_python_piratas/piratas.py
+++ 20100805_python_piratas/piratas.py
@@ -8,4 +8,7 @@ def divide_tesouro(tesouro, numero_piratas):
     if total_das_moedas % numero_piratas:
         return False
 
-    return sum(valor_das_moedas) / numero_piratas
+    valor_dividido = sum(valor_das_moedas) / numero_piratas
+    if max(tesouro.keys()) > valor_dividido:
+        return False
+    return valor_dividido
