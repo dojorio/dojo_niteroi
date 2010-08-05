0cad1f686cec4ee2fe5753a040bf268c6ba40771
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-05 19:37:49 -0300

    Thu Aug  5 19:37:49 2010

--- 20100805_python_piratas/test_piratas.py
+++ 20100805_python_piratas/test_piratas.py
@@ -24,9 +24,10 @@ class TestPiratas(unittest.TestCase):
         piratas = 1
         self.assertEqual(divide_tesouro(tesouro, piratas), 6)
 
-    def test_2_moeda_de_3_para_1_pirata(self):
+    def test_1_moeda_de_3_e_uma_de_5_para_1_pirata(self):
         tesouro = {
-            3: 2,
+            3: 1,
+            5: 1,
         }
         piratas = 1
         self.assertEqual(divide_tesouro(tesouro, piratas), 6)
