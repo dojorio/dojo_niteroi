bb2771e6f544167b08e7c9a7e4bf08188bcdb440
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-05 19:26:20 -0300

    Thu Aug  5 19:26:20 2010

---
+++ 20100805_python_piratas/piratas.py
@@ -0,0 +1,2 @@
+def piratas(entrada):
+    pass
---
+++ 20100805_python_piratas/test_piratas.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestPiratas(unittest.TestCase):
+
+    def test_soma(self):
+        self.assertEqual(1, 1)
+
+unittest.main()
