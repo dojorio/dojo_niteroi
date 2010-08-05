cb41838a4d8df4f08648844f0b9af60b435afbf0
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-05 19:39:25 -0300

    Thu Aug  5 19:39:24 2010

--- 20100805_python_piratas/piratas.py
+++ 20100805_python_piratas/piratas.py
@@ -2,5 +2,5 @@ def divide_tesouro(tesouro, numero_piratas):
     valor_por_pirata = 0
     moeda = tesouro.keys()[0]
     for moeda, quantidade in tesouro.items():
-        valor_por_pirata += tesouro[moeda] * moeda
-    return moeda * tesouro[moeda]
+        valor_por_pirata += quantidade * moeda
+    return valor_por_pirata
