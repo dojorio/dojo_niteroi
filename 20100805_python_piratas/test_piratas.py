4e661fc69f761ca84919c178bc25a89745cc80e3
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-05 19:34:15 -0300

    Thu Aug  5 19:34:15 2010

--- 20100805_python_piratas/test_piratas.py
+++ 20100805_python_piratas/test_piratas.py
@@ -2,7 +2,11 @@ import unittest
 
 class TestPiratas(unittest.TestCase):
 
-    def test_soma(self):
-        self.assertEqual(1, 1)
+    def test_1_moeda_para_1_pirata(self):
+        tesouro = {
+            5: 1,
+        }
+        piratas = 1
+        self.assertEqual(divide_tesouro(tesouro, piratas), 5)
 
 unittest.main()
