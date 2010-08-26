1d6f6a024ce13347dcc37d7eb795a7d67dc2ec29
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-26 20:14:14 -0300

    Thu Aug 26 20:14:14 2010

--- 20100826_ruby_ascii_grid/ascii_grid.rb
+++ 20100826_ruby_ascii_grid/ascii_grid.rb
@@ -2,8 +2,10 @@ def conversor(list)
   if list[0].empty?
     ""
   else
-    text = " " * (list[0][2] + 1)
-    text[list[0][2]] = list[0][0].chr
-    text.rstrip
+    for coord in list
+      text = " " * (coord[2] + 1)
+      text[coord[2]] = coord[0].chr
+      text.rstrip
+    end
   end
 end
