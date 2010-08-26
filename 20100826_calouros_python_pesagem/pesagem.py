d67ad5903c0d0acc7deda4bce832a8a9cc8c2fd5
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-26 11:49:24 -0300

    Thu Aug 26 11:49:24 2010

--- 20100826_calouros_python_pesagem/pesagem.py
+++ 20100826_calouros_python_pesagem/pesagem.py
@@ -1,6 +1,11 @@
 # -*- coding:utf-8 -*-
 import unittest
 
+def pesa_pedras(pedras, peso_ideal)
+    return 'Não é possível'
+
+
+
 class TestPesagemDasPedras(unittest.TestCase):
 
     def test_0_pedras_e_peso_ideal_1kg_retorna_nao_e_possivel(self):
