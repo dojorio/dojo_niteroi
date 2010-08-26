dc106fc6bb8989476aa79bc2a86cf3b5315eef2a
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-26 11:37:46 -0300

    Thu Aug 26 11:37:46 2010

--- 20100826_calouros_python_pesagem/pesagem.py
+++ 20100826_calouros_python_pesagem/pesagem.py
@@ -2,6 +2,6 @@ import unittest
 
 class TestPesagemDasPedras(unittest.TestCase):
 
-    def test_0_pedras_e_peso_ideal_1kg_retorna_nao_e_possivel(self)
+    def test_0_pedras_e_peso_ideal_1kg_retorna_nao_e_possivel(self):
         pedras = {}
         self.assertEquals(pesa_pedras(pedras, 1), 'Não é possível')
