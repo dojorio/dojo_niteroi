d988ecdcb826be2fd19de899c36a544339a3150f
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-26 20:07:21 -0300

    Thu Aug 26 20:07:21 2010

--- 20100826_ruby_ascii_grid/ascii_grid_spec.rb
+++ 20100826_ruby_ascii_grid/ascii_grid_spec.rb
@@ -5,23 +5,23 @@ require 'ascii_grid'
 describe "Conversor" do
 
   it 'should return an empty string when list is empty' do
-    conversor([]).should == ""
+    conversor([[]]).should == ""
   end
 
   it "should be 'A' when input is [[65, 0, 0]]" do
-    conversor([65, 0, 0]).should == "A"
+    conversor([[65, 0, 0]]).should == "A"
   end
 
   it "should be 'B' when input is [[66, 0, 0]]" do
-    conversor([66, 0, 0]).should == "B"
+    conversor([[66, 0, 0]]).should == "B"
   end
 
   it "should be ' A' when input is [[65, 0, 1]]" do
-    conversor([65, 0, 1]).should == " A"
+    conversor([[65, 0, 1]]).should == " A"
   end
 
   it "should be '    A' when input is [[65, 0, 4]]" do
-    conversor([65, 0, 4]).should == "    A"
+    conversor([[65, 0, 4]]).should == "    A"
   end
 
 end
