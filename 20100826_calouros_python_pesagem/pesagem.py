834227c9c3d745b838ca1b9ac1859b38af98a13b
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-26 12:32:13 -0300

    Thu Aug 26 12:32:13 2010

--- 20100826_calouros_python_pesagem/pesagem.py
+++ 20100826_calouros_python_pesagem/pesagem.py
@@ -28,13 +28,13 @@ class TestPesagemDasPedras(unittest.TestCase):
 
     def test_2_pedras_de_1kg_e_peso_ideal_2kg_retorna_2(self):
         pedras = {
-            "1 kg":2
+            Pedra (1, "kg"):2
         }
         self.assertEquals(pesa_pedras(pedras, 2), 2)
 
     def test_1_pedras_de_2kg_e_peso_ideal_2kg_retorna_1(self):
         pedras = {
-            "2 kg":1
+            Pedra (2, "kg"):1
         }
         self.assertEquals(pesa_pedras(pedras,2),1)
 
