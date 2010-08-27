700387cafc3614e205a84f458726d822b6253c74
Bernardo Fontes <bernardoxhc@gmail.com>
2010-08-26 21:00:03 -0300

    Thu Aug 26 21:00:03 2010

--- 20100826_ruby_ascii_grid/ascii_grid.rb
+++ 20100826_ruby_ascii_grid/ascii_grid.rb
@@ -1,15 +1,17 @@
 def conversor(list)
-  if list[0] == nil
+  first_character = list[0]
+
+  if primeiro_caracter == nil
     return ""
   end
 
-  if list[0].empty?
+  if primeiro_caracter.empty?
     ""
   else
-    column_of_first_input_line = list[0][2]
+    column_of_first_input_line = primeiro_caracter[2]
     text = " " * (column_of_first_input_line + 1)
 
-    char_code_of_first_input_line = list[0][0]
+    char_code_of_first_input_line = primeiro_caracter[0]
     text[column_of_first_input_line] = char_code_of_first_input_line.chr
     text.rstrip
 
