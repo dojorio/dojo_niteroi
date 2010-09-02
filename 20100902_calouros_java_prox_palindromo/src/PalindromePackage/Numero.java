a32d865bbae972e70a0d6d40b53e5c0e0e60b90c
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-02 12:52:47 -0300

    Dojo Calouros Próximo Palíndromo

--- 20100902_calouros_java_prox_palindromo/src/PalindromePackage/Numero.java
+++ 20100902_calouros_java_prox_palindromo/src/PalindromePackage/Numero.java
@@ -1,18 +1,37 @@
 package PalindromePackage;
 
+import java.util.ArrayList;
+
 public class Numero {
 
     private int value;
 
-    public Numero(){
+    public Numero() {
     }
 
-    public Numero(int value){
+    public Numero(int value) {
         this.value = value;
     }
 
-    public int nextPalidrome(){
-        return 0;
+    public int nextPalidrome() {
+
+        do{
+            ++value;
+        }
+        while(!isPalindrome());
+        return value;
+
+
     }
 
+    public boolean isPalindrome()
+    {
+        String strNumero = value+"";
+        for(int i = 0; i < strNumero.length()/2; i++){
+            ArrayList hey = new ArrayList();
+            if(strNumero.charAt(i) != strNumero.charAt(strNumero.length()-i-1))
+                return false;
+        }
+        return true;
+    }
 }
--- 20100902_calouros_java_prox_palindromo/test/TestPalindromePackage/NumeroTest.java
+++ 20100902_calouros_java_prox_palindromo/test/TestPalindromePackage/NumeroTest.java
@@ -11,9 +11,49 @@ import static org.junit.Assert.*;
 
 public class NumeroTest {
     @Test
-    public void testNextPalidrome() {
-        Numero instance = new Numero();
-        assertEquals(0, instance.nextPalidrome());
+    public void testRecebe0Retorna1() {
+        Numero instance = new Numero(0);
+        assertEquals(1, instance.nextPalidrome());
+    }
+    
+    @Test
+    public void testRecebe1Retorna2() {
+        Numero instance = new Numero(1);
+        assertEquals(2, instance.nextPalidrome());
     }
 
+    @Test
+    public void testRecebe9Retorna11() {
+        Numero instance = new Numero(9);
+        assertEquals(11, instance.nextPalidrome());
+    }
+
+    @Test
+    public void testRecebe11Retorna22() {
+        Numero instance = new Numero(11);
+        assertEquals(22, instance.nextPalidrome());
+    }
+
+    @Test
+    public void testRecebe23Retorna33() {
+        Numero instance = new Numero(23);
+        assertEquals(33, instance.nextPalidrome());
+    }
+
+    @Test
+    public void testeFuncaoIsPalindromeRetornaFalse() {
+        Numero instance = new Numero(23);
+        assertEquals(false, instance.isPalindrome());
+    }
+
+    @Test
+    public void testeFuncaoIsPalindromeRetornaTrue() {
+        Numero instance = new Numero(2);
+        assertEquals(true, instance.isPalindrome());
+    }
+    @Test
+    public void testeFuncaoIsPalindromeRetornaF() {
+        Numero instance = new Numero(24567);
+        assertEquals(false, instance.isPalindrome());
+    }
 }
