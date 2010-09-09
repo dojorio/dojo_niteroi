57cb1ca2ebeec8ef86c2c5efd438e68f2da7b019
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-09 12:31:06 -0300

    Thu Sep  9 12:31:06 2010

--- 20100909_calouros_python_tri_pascal/test_tri_pascal.py
+++ 20100909_calouros_python_tri_pascal/test_tri_pascal.py
@@ -40,4 +40,7 @@ class TestCombinacao(unittest.TestCase):
 
     def test_combinacao_de_2_2_a_2(self):
         self.assertEqual(combinacao(2, 2), 1)
+
+    def test_combinacao_de_3_1_a_1(self):
+        self.assertEqual(combinacao(3, 1), 3)
 unittest.main()
