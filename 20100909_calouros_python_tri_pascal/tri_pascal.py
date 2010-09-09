1b81fe91a7cd35af4b6e0382e71344f836068a40
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-09 11:42:41 -0300

    Thu Sep  9 11:42:41 2010

--- 20100909_calouros_python_tri_pascal/tri_pascal.py
+++ 20100909_calouros_python_tri_pascal/tri_pascal.py
@@ -1,4 +1,5 @@
 def triangulo_pascal(num_linhas):
+
     if num_linhas == 1:
         return [
             [1],
@@ -8,8 +9,14 @@ def triangulo_pascal(num_linhas):
             [1],
             [1, 1],
         ]
+    elif num_linhas == 3:]
+        return [
+            [1],
+            [1, 1],
+            [1, 2, 1],
+        ]
     return [
         [1],
         [1, 1],
-        [1, 2, 1],
+        [1, 3, 3, 1],
     ]
