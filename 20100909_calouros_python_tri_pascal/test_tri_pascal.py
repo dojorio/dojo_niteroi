934800a57c5f4159ef22871645a6f568513ea615
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-09 11:34:11 -0300

    Thu Sep  9 11:34:11 2010

---
+++ 20100909_calouros_python_tri_pascal/test_tri_pascal.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestTrianguloPascal(unittest.TestCase):
+
+    def test_triangulo_1_linha(self):
+        self.assertEqual(triangulo_pascal(1), [
+            [1],
+        ])
