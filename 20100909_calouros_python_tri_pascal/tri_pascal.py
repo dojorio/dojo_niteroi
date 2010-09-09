3f55784ba7e1b0bd31e0cb146199407d81cfcab9
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-09 13:06:58 -0300

    Thu Sep  9 13:06:58 2010

--- 20100909_calouros_python_tri_pascal/tri_pascal.py
+++ 20100909_calouros_python_tri_pascal/tri_pascal.py
@@ -3,28 +3,6 @@ from math import factorial as fat
 def triangulo_pascal(num_linhas):
     return [gera_linha(i) for i in range(1, num_linhas+1)]
 
-    if num_linhas == 1:
-        return [
-            [1],
-        ]
-    elif num_linhas == 2:
-        return [
-            [1],
-            [1, 1],
-        ]
-    elif num_linhas == 3:
-        return [
-            [1],
-            [1, 1],
-            [1, 2, 1],
-        ]
-    return [
-        [1],
-        [1, 1],
-        [1, 2, 1],
-        [1, 3, 3, 1],
-    ]
-
 def combinacao(n, p):
     return fat(n) / (fat(n-p) * fat(p))
 
