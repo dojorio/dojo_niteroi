dfaae16f428dfea010616e1a53ce3d187e823cc3
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-16 11:24:22 -0300

    Thu Sep 16 11:24:22 2010

--- 20100916_calouros_python_num_romanos_inverso/test_romanos.py
+++ 20100916_calouros_python_num_romanos_inverso/test_romanos.py
@@ -5,7 +5,11 @@ from romanos import NumeroRomano
 class TestRomanos(unittest.TestCase):
 
     def test_I_deve_retornar_1(self):
-        romano = NumeroRomano('I')
-        self.assertEqual(romano.to_int(), 1)
+        numero_romano = NumeroRomano('I')
+        self.assertEqual(numero_romano.to_int(), 1)
+
+    def test_II_deve_retornar_2(self):
+        numero_romano = NumeroRomano('II')
+        self.assertEqual(numero_romano.to_int(), 2)
 
 unittest.main()
