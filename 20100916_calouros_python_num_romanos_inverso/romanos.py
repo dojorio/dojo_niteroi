2a4456b55a4f56e09419107996f392d14229db09
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-16 11:40:13 -0300

    Thu Sep 16 11:40:13 2010

--- 20100916_calouros_python_num_romanos_inverso/romanos.py
+++ 20100916_calouros_python_num_romanos_inverso/romanos.py
@@ -4,11 +4,11 @@ class NumeroRomano(object):
         self.valor = valor
 
     def to_int(self):
-        #if self.valor == 'X':
-           # return 10
         tamanho = len(self.valor)
         if self.valor[-1] == 'X':
             tamanho = 11 - tamanho
+        elif self.valor[0] == 'X':
+            tamanho = 9 + tamanho
         if self.valor[-1] == 'V':
             tamanho = 6 - tamanho
         elif self.valor[0] == 'V':
