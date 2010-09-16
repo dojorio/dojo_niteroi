e6d184dd877d679192715b76cb94544387600451
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-16 12:31:26 -0300

    Thu Sep 16 12:31:26 2010

--- 20100916_calouros_python_num_romanos_inverso/romanos.py
+++ 20100916_calouros_python_num_romanos_inverso/romanos.py
@@ -11,7 +11,7 @@ class NumeroRomano(object):
     def to_int(self):
         resultado = 0
         for i, caractere in enumerate(self.valor):
-            if i<len(self.valor-2) and self.dic[self.valor[i+1]] > self.dic[caractere]:
+            if i<len(self.valor)-1 and self.dic[self.valor[i+1]] > self.dic[caractere]:
                 resultado -= self.dic[caractere]
             else:
                 resultado += self.dic[caractere]
