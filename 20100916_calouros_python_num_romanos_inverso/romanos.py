9cf1bc8b6e7403dba613a330556b72e676f7ed6f
Bernardo Fontes <bernardoxhc@gmail.com>
2010-09-16 12:31:35 -0300

    Thu Sep 16 12:31:35 2010

--- 20100916_calouros_python_num_romanos_inverso/romanos.py
+++ 20100916_calouros_python_num_romanos_inverso/romanos.py
@@ -16,17 +16,3 @@ class NumeroRomano(object):
             else:
                 resultado += self.dic[caractere]
         return resultado
-
-
-        tamanho = len(self.valor)
-        ultimo = self.valor[-1]
-        primeiro = self.valor[0]
-
-        if ultimo == 'V' and primeiro == 'X':
-            tamanho = 17 - tamanho
-        elif ultimo == 'X' or ultimo == 'V':
-            tamanho = self.dic[ultimo] - tamanho + 1
-        elif primeiro == 'X' or primeiro == 'V':
-            tamanho = self.dic[primeiro] + tamanho - 1
-
-        return tamanho
