b2ed6030a247333747bdebcab1631bbfee7f80f9
Ayres <dojotools@dojo>
2011-06-16 21:38:02 -0300

    Thu Jun 16 21:38:02 2011

--- 20110616_ruby_brainfuck/brainfuck.rb
+++ 20110616_ruby_brainfuck/brainfuck.rb
@@ -1,15 +1,18 @@
 def brainfuck(codigo, input)
   i = -1
   valor = 0
+  retorno = ""  
   codigo.split("").each do |c|
+    
     if c == ','
       valor = input[i = (i+1)]
     elsif ['+', '-'].include? c 
       valor = valor.send(c, 1)
+    elsif c == "."
+      retorno += valor.to_s  
     end
-    
-    if c == '.'
-       valor = input[-1]    
+   
+     
   end
-  return valor.to_s
+  retorno
 end
