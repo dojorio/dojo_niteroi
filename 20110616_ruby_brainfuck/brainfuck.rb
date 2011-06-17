e021afdac74fef956ad1b4690001f86eb6aee969
Gerardo <dojotools@dojo>
2011-06-16 21:15:14 -0300

    Thu Jun 16 21:15:13 2011

--- 20110616_ruby_brainfuck/brainfuck.rb
+++ 20110616_ruby_brainfuck/brainfuck.rb
@@ -1,8 +1,21 @@
 def brainfuck(codigo, input)
-  if codigo == ',+,.'
-    return "12"
-  end
+  #if codigo == ',+,.'
+   # return "12"
+  #end
 
-  valor = input[codigo.count(',')-1] + codigo.count('+') - codigo.count('-')
-  valor.to_s
+  #valor = input[codigo.count(',')-1] + codigo.count('+') - codigo.count('-')
+  #valor.to_s
+  i = -1
+  valor = 0
+  codigo.to_a.each do |c|
+    if c == ','
+      i+=1
+      valor = input[i]
+    elsif c == '+'
+      valor+=1
+    elsif c == '-'
+      valor-=1
+    end
+  end
+  return valor.to_s
 end
